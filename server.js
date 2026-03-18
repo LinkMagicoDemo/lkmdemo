c36d264f31350626e55dd7a7d89afd52d6ed5051
Edvaldols1231 <brendjjs@gmail.com-Edvaldols1231>
2026-03-18 01:12:12 -0300

    🔧 Serve static assets (1 linha) para imagens de prova social

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ app.use(helmet({
     }
 }));
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 
